d9823cd13b897fef049c4209cf69f3bf9d205dc5
Polowis <tranphuochung0912@gmail.com>
2019-06-02 13:43:33 +1000

    Initial commit

--- src/location.js
+++ src/location.js
@@ -10,24 +10,27 @@ class location{
     }
     
 }
-class location extends state{
-    constructor(name, year){
+class state extends location{
+    constructor(name, year = 2017){
         super(name, year)
         this.name = name;
         this.year = year;
     }
     async getAllPopulation(){
-        const population = await fetch(`https://datausa.io/api/data?drilldowns=State&measures=Population&year=${this.year}`)
+        const population = await fetch(`https://datausa.io/api/data?drilldowns=State&measures=Population`)
         .then(response => response.json())
-        console.log(population.data[0].population)
+        
+        console.log(population)
     }
     getPopulation(){
 
     }
     
 }
-class location extends msa{
+class msa extends location{
     constructor(){
         super()
     }
 }
+const result = new state()
+result.getAllPopulation()
